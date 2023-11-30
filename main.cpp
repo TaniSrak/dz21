36a8f45967b034b006bbdc25b128372ba3a24496
TaniSrak <kozhevnikova.korova.99@mail.ru>
2023-11-30 14:28:52 +0500

    Добавьте файлы проекта.

---
+++ main.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int x, y;
+	std::cout << "Введите обозначение для х и y -> ";
+	std::cin >> x >> y;
+
+	int* px = &x;
+	int* py = &y;
+
+	int peremena = *px;
+	*px = *py;
+	*py = peremena;
+
+	std::cout << " x = " << x << std::endl;
+	std::cout << " y = " << y << std::endl;
+
+	//   =%
+
+	return 0;
+}
